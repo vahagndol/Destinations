cfb8af57b6cb6488c59047437ecd433226a3aa87
vahagn.dolbakyan <vahagn.dolbakyan@outlook.com>
2018-07-10 02:13:52 +0200

    Updated places component in Application

--- src/Application/ClientApp/src/app/places/places.component.ts
+++ src/Application/ClientApp/src/app/places/places.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-places',
   templateUrl: './places.component.html',
   styleUrls: ['./places.component.css']
 })
-export class PlacesComponent implements OnInit {
+export class PlacesComponent {
+  public places: Places[];
+  public cityName: string;
+  public cityId: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+    http: HttpClient,
+    @Inject('BASE_URL') baseUrl: string) {
+    this.cityId = this.route.snapshot.paramMap.get("cityId");
+    this.cityName = this.route.snapshot.paramMap.get("cityName");
+    http.get<Places[]>(baseUrl + 'api/places/GetAll/' + this.cityId).subscribe(result => {
+      this.places = result;
+      },
+      error => console.error(error));
   }
+}
 
+interface Places {
+  id: string;
+  name: string;
+  summary: string;
+  imageUri: string;
 }

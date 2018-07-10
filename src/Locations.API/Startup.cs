bb19c2fc0eae6adbf73a29af0ff7915f7c3a6c26
Vahagn Dolbakyan <vahagn.dolbakyan@pds.nl>
2018-07-10 13:44:28 +0200

    Added Swagger for API projects

--- src/Locations.API/Startup.cs
+++ src/Locations.API/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using System.Linq;
+using Domain.Entities;
 using Infrastructure.Context;
 using Infrastructure.Repositories;
 using Infrastructure.Services;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Locations.API
 {
@@ -31,6 +33,21 @@ namespace Locations.API
             services.AddSingleton<IRepository<Location>, Repository<Location>>();
             services.AddSingleton<IEntityService<Location>, EntityService<Location>>();
 
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.DocInclusionPredicate((version, apiDescription) =>
+                {
+                    var values = apiDescription.RelativePath
+                        .Split('/')
+                        .Select(v => v.Replace("v{version}", version));
+
+                    apiDescription.RelativePath = string.Join("/", values);
+                    return true;
+                });
+                setupAction.SwaggerDoc("v1", new Info { Title = "Locations API", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -43,6 +60,15 @@ namespace Locations.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/v1/swagger.json", "Locations API V1");
+            });
 
             app.UseMvc();
         }
